d8579389317d4c06d14a95d0b7eca08a1d91002d
Jakub Bokoc <jakub@awesomedevelopers.eu>
2018-10-10 13:27:48 +0200

    fixed import_driver

--- adsocket_transport/utils.py
+++ adsocket_transport/utils.py
@@ -7,7 +7,7 @@ def import_driver(name):
     :param pth:
     :return:
     """
-    the_module = importlib.import_module(f'broker.{name}')
+    the_module = importlib.import_module(f'adsocket_transport.broker.{name}')
     class_ptr = getattr(the_module, 'broker')
 
     return class_ptr
